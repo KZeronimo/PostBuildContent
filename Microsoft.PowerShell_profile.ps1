b3e7be57b76326cc8b87ef7bc107b4953b177b0f
KZeronimo <KZeronimo@users.noreply.github.com>
2018-05-18 09:31:26 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
-﻿# Only load oh-my-posh if PS is running from ConEmu
+# Only load oh-my-posh if PS is running from ConEmu
 if ($env:ConEmuTask -or $env:ConEmuPID)
 {
     # Import oh-my-posh and dependecies
     Import-Module -Name posh-git -ErrorAction SilentlyContinue
     Import-Module -Name oh-my-posh
+	# Shorten prompt by providing your uesername
+	$DefaultUser = $env:UserName
 }
 else
 {
@@ -32,3 +34,4 @@ Function Up1Dir { cd ..; }; Set-Alias -Name .. -Value Up1Dir
 Function Up2Dir { cd ../..; }; Set-Alias -Name ... -Value Up2Dir
 Function SandboxSourceDir { cd c:/_Src/Sdbx; ls; }; Set-Alias -Name sdbx -Value SandboxSourceDir
 Function ProdSourceDir { cd c:/_Src/Prod; ls; }; Set-Alias -Name prod -Value ProdSourceDir
+Function DotFileGit { & git --git-dir=$env:UserProfile/.myconf/ --work-tree=$env:UserProfile $args; }; Set-Alias -Name cfg -Value DotFileGit
